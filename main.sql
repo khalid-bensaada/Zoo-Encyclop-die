c910eb4640575f5c3fd885461158238aa5ba6009
khalid-bensaada <kbensaada953@gmail.com>
2025-12-02 16:48:28 +0100

    added the data of if user add many animales

--- main.sql
+++ main.sql
@@ -18,7 +18,14 @@ CREATE TABLE animal(
     FOREIGN KEY (habita_id) REFERENCES habitats(id_hab)
 );
 
-/*added the data of animales*/
+/*added the data of if user add one animale*/
 INSERT INTO animal (id_animal, name_animal, image_animal)
 VALUES (1 , "lion" , "https://tse1.mm.bing.net/th/id/OIP.T-SQ5d8n6_jhLDOj3yx7_wHaEK?pid=Api&P=0&h=180 ");
+
+/*added the data of if user add many animales*/
+INSERT INTO animal ( name_animal, image_animal)
+VALUES ( "lion" , "https://tse1.mm.bing.net/th/id/OIP.T-SQ5d8n6_jhLDOj3yx7_wHaEK?pid=Api&P=0&h=180 "),
+( "fish", "https://tse4.mm.bing.net/th/id/OIP.xRrB_f7LSd3AfjjqYR6pDAHaEK?pid=Api&P=0&h=180"),
+("tiger","https://tse3.mm.bing.net/th/id/OIP.wYKbiPYohf2SKxF02sSgHAHaEJ?pid=Api&P=0&h=180"),
+( "snake","https://tse1.mm.bing.net/th/id/OIP.bdTDfzNFuYC1AUrd0TvhtgHaEK?pid=Api&P=0&h=180");
 
